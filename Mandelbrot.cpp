cf8f3de0ccd286de18c517a4c36d0568918f818b
Ryan <RyanK.BusinessMail@gmail.com>
2018-03-21 13:42:47 -0400

    A class that houses the Mandelbrot algorithm

---
+++ Mandelbrot.cpp
@@ -0,0 +1,42 @@
+//
+//
+//
+// Mandelbrot.cpp
+
+// Notes:   x
+
+#include <complex>
+#include "Mandelbrot.h"
+using namespace fractal;
+using namespace std;
+
+
+
+namespace fractal {
+
+Mandelbrot::Mandelbrot() {}
+Mandelbrot::~Mandelbrot() {}
+
+int Mandelbrot::getIterations(double x, double y) {
+
+    complex<double> z = 0;
+    complex<double> c(x, y);
+
+    int iterations = 0;
+
+    while(iterations < MAX_ITERATIONS) {
+        z = z * z + c;
+
+        if(abs(z) > 2) {
+            break;
+        }
+
+        iterations++;
+    }
+
+    return iterations;
+}
+
+
+
+} // namespace fractal
