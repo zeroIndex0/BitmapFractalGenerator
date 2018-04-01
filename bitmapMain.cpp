aaf19a8698e3631091e54160c4a3390ca8d27ece
Ryan <RyanK.BusinessMail@gmail.com>
2018-03-31 21:22:55 -0400

    this is Main
    Just updated some of the info on this file in case someone wants to mess around with it and doesn't want to read each file to figure out what the functions are doing in this section.  Or at least, I attempted to clarify some of whats going on.  That someone can mess around after all the files are linked up in a project

--- bitmapMain.cpp
+++ bitmapMain.cpp
@@ -25,6 +25,10 @@ int main(void) {
 
     //Caution: this is really fragile with the numbers we are adding in and could easily break this program
     //so to fix that we would need to create a function that checks the error conditions.
+    //in this case the first number is the rage of iterations based in the hundreds
+    //0.0 -> 0.3 means the portions of the fractal that take   0 - 300 iterations to generate the pixel
+    //0.3 -> 0.5 means the portions of the fractal that take 300 - 500 iterations to generate the pixel
+    //so messing around with those range values could cause some issues, the colors are fine to change to whatever you want
     fractalCreator.addRange(0.0, RGB(0, 0, 0));
     fractalCreator.addRange(0.3, RGB(255, 0, 0));
     fractalCreator.addRange(0.5, RGB(255, 255, 0));
@@ -32,6 +36,10 @@ int main(void) {
 
 
 
+    //the numbers found here are what pixel you want to zoom in on.  You can find the pixel location by loading the image
+    //into a photo editor and putting your cursor over a point and seeing what the pixel location is
+    //its easy to find in gimp and not so easy to find in photoshop, but it is there
+    //and the thrid value is the scale at which you want to zoom, adjusting this can give some strange results, but maybe you want that
     fractalCreator.addZoom(Zoom(width/2, height/2, ZOOM_SIZE / width));
     fractalCreator.addZoom(Zoom(295, 202, 0.1));
     fractalCreator.addZoom(Zoom(312, 304, 0.1));
