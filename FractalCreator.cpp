bd7cf82145afe7c725b0389d6ff776f7c0e4c1ec
Ryan <RyanK.BusinessMail@gmail.com>
2018-04-14 12:34:43 -0400

    class that houses all the fractal build functions
    Added the first fractal zoom to the constructor to give it a default image if you were to run the program with no zoom assigned

--- FractalCreator.cpp
+++ FractalCreator.cpp
@@ -67,6 +67,7 @@ FractalCreator::FractalCreator(int width, int height) :
     m_fractal(new int[m_width * m_height]{}),
     m_bitmap(m_width, m_height),
     m_zoomList(m_width, m_height){
+        m_zoomList.add(Zoom(m_width / 2, m_height / 2, 4.0 / m_width)); //The 4.0 is the zoom size
 }
 
 FractalCreator::~FractalCreator(){}
